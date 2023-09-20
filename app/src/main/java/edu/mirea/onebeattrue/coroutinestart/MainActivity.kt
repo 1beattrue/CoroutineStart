58027623656745b8206d8d5a92b2404b1d2e104c
Vitaly <onebeattrue@gmail.com>
2023-09-21 02:23:21 +0300

    Добавление каркаса

--- app/src/main/java/edu/mirea/onebeattrue/coroutinestart/MainActivity.kt
+++ app/src/main/java/edu/mirea/onebeattrue/coroutinestart/MainActivity.kt
@@ -2,9 +2,14 @@ package edu.mirea.onebeattrue.coroutinestart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import edu.mirea.onebeattrue.coroutinestart.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
 
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
@@ -13,5 +18,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        viewModel.method()
     }
 }
---
+++ app/src/main/java/edu/mirea/onebeattrue/coroutinestart/MainViewModel.kt
@@ -0,0 +1,7 @@
+package edu.mirea.onebeattrue.coroutinestart
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    fun method() {}
+}
