ccd36b80fe429463651684c3cdb166e6143e9d9c
Vitaly <onebeattrue@gmail.com>
2023-09-21 02:38:29 +0300

    Правила Structured Concurrency: 1) Все coroutine'ы должны быть запущены внутри scope'а с определенным жизненным циклом 2) Все запущенные coroutine'ы располагаются в виде иерархии объектов Job 3) Родительский Job будет активен, пока дочерние не завершат работу 4) Если родительский Job отменяется (cancel()), то отменяются и все дочерние coroutine'ы (в обратную сторону не работает) 5) в следующем уроке...

--- app/src/main/java/edu/mirea/onebeattrue/coroutinestart/MainViewModel.kt
+++ app/src/main/java/edu/mirea/onebeattrue/coroutinestart/MainViewModel.kt
@@ -1,7 +1,46 @@
 package edu.mirea.onebeattrue.coroutinestart
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.concurrent.thread
 
 class MainViewModel : ViewModel() {
-    fun method() {}
+
+    private val parentJob = Job()
+    private val coroutineScope = CoroutineScope(Dispatchers.Main + parentJob)
+
+    fun method() {
+        val childJob1 = coroutineScope.launch {
+            delay(3000)
+            Log.d(LOG_TAG, "first coroutine finished")
+        }
+        val childJob2 = coroutineScope.launch {
+            delay(2000)
+            Log.d(LOG_TAG, "second coroutine finished")
+        }
+
+        thread {
+            Thread.sleep(1000)
+            // parentJob.cancel() // отменятся также дочерние
+            Log.d(LOG_TAG, "parentJob is active: ${parentJob.isActive}")
+        }
+
+        Log.d(LOG_TAG, parentJob.children.contains(childJob1).toString())
+        Log.d(LOG_TAG, parentJob.children.contains(childJob2).toString())
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        coroutineScope.cancel()
+    }
+
+    companion object {
+        private const val LOG_TAG = "MainViewModel"
+    }
 }
